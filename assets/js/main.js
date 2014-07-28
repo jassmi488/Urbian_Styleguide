f37f7a1b59f19c84bb2b2bfc3e1db72302781545
Michael Thorne <michael.thorne@gmail.com>
2014-07-28 18:04:34 +0200

    Added: elements, forms, patterns, templates

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,45 @@
 
         init: function ()
         {
+            /* ==========================================================================
+               Map
+               ========================================================================== */
 
+            renderMap();
+
+            /**
+             * Render the static map image or embedded iFrame depending on the screen width
+             */
+
+            function renderMap()
+            {
+                var map = $('[data-component="map"]');
+
+                if (map.length > 0)
+                {
+                    var screen_width = document.body.clientWidth;
+                    var breakpoint = 640; // This is the equivalent of `@bp-xs`
+                    var url_embed = map.data('url-embed');
+                    var map_img = $('.map__img', map);
+                    var map_canvas = $('.map__canvas', map);
+                    var map_button = $('.btn', map);
+
+                    if (screen_width >= breakpoint)
+                    {
+                        map_img.addClass('is-hidden');
+                        map_button.addClass('is-hidden');
+                        map_canvas.attr('src', url_embed);
+                        map_canvas.removeClass('is-hidden');
+                    }
+                    else
+                    {
+                        map_img.removeClass('is-hidden');
+                        map_button.removeClass('is-hidden');
+                        map_canvas.attr('src', '');
+                        map_canvas.addClass('is-hidden');
+                    }
+                }
+            }
         }
     },
 
