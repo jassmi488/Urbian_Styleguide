5da974379dfac181b34dd66b6dc19dc15e75b605
Michael Thorne <michael.thorne@userx.co.za>
2015-05-21 12:20:01 +0200

    Formatted `main.js` according to ESLint

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,8 +2,7 @@
  * Main JavaScript
  * ======================================================================== */
 
-(function ()
-{
+(function () {
     'use strict';
 
     /*
@@ -12,7 +11,7 @@
      * Based on: http://responsivenews.co.uk/post/18948466399/cutting-the-mustard
      */
 
-    var isModernBrowser = ('addEventListener' in window) ? true : false;
+    var isModernBrowser = ('addEventListener' in window);
 
     /* ==========================================================================
      * Accordion
@@ -20,15 +19,12 @@
 
     var $accordion_component = $('.js-accordion');
 
-    if ($accordion_component.length > 0)
-    {
-        $accordion_component.each(function ()
-        {
+    if ($accordion_component.length > 0) {
+        $accordion_component.each(function () {
             var $accordion = $(this);
             var $accordion_link = $('.accordion-item__link', $accordion);
 
-            $accordion_link.on('click', function (e)
-            {
+            $accordion_link.on('click', function (e) {
                 e.preventDefault();
 
                 var $link = $(this);
@@ -41,14 +37,12 @@
                  * Update the selected accordion item
                  */
 
-                if ($item.hasClass('is-selected'))
-                {
+                if ($item.hasClass('is-selected')) {
                     $item.removeClass('is-selected');
                     $item.attr('aria-hidden', 'true').attr('aria-selected', 'false');
                     $icon.removeClass(icon_up).addClass(icon_down);
                 }
-                else
-                {
+                else {
                     $item.addClass('is-selected');
                     $item.attr('aria-hidden', 'false').attr('aria-selected', 'true');
                     $icon.removeClass(icon_down).addClass(icon_up);
@@ -63,16 +57,13 @@
 
     var $alert_component = $('.js-alert');
 
-    if ($alert_component.length > 0)
-    {
-        $alert_component.each(function ()
-        {
+    if ($alert_component.length > 0) {
+        $alert_component.each(function () {
             var $alert = $(this);
 
             var $dismiss_alert = $('.js-dismiss', $alert);
 
-            $dismiss_alert.on('click', function (e)
-            {
+            $dismiss_alert.on('click', function (e) {
                 e.preventDefault();
 
                 $alert.fadeOut();
@@ -86,10 +77,8 @@
 
     var $carousel_component = $('.js-carousel');
 
-    if ($carousel_component.length > 0)
-    {
-        $carousel_component.each(function ()
-        {
+    if ($carousel_component.length > 0) {
+        $carousel_component.each(function () {
             var $carousel = $(this);
 
             $carousel.slick({
@@ -106,12 +95,10 @@
      * Render the static map image or embedded iframe depending on the screen width
      */
 
-    function renderMapEmbed()
-    {
+    function renderMapEmbed () {
         var $map_embed = $('.js-map-embed');
 
-        if ($map_embed.length > 0)
-        {
+        if ($map_embed.length > 0) {
             var screen_width = document.body.clientWidth;
             var breakpoint_xs = 480; // This is the equivalent of `@bp-xs`
             var url_embed = $map_embed.data('url-embed');
@@ -119,15 +106,15 @@
             var $map_embed_canvas = $('.map-embed__canvas', $map_embed);
             var $map_embed_button = $('.btn', $map_embed);
 
-            if (screen_width >= breakpoint_xs) // Display the interactive embedded map
-            {
+            if (screen_width >= breakpoint_xs) {
+                // Display the interactive embedded map
                 $map_embed_img.addClass('is-hidden');
                 $map_embed_button.addClass('is-hidden');
                 $map_embed_canvas.attr('src', url_embed);
                 $map_embed_canvas.removeClass('is-hidden');
             }
-            else // Display a static image of the map
-            {
+            else {
+                // Display a static image of the map
                 $map_embed_img.removeClass('is-hidden');
                 $map_embed_button.removeClass('is-hidden');
                 $map_embed_canvas.attr('src', '');
@@ -144,36 +131,30 @@
 
     var $nav_dropdown = $('.js-nav-dropdown');
 
-    if ($nav_dropdown.length > 0)
-    {
-        $('a', $nav_dropdown).each(function ()
-        {
+    if ($nav_dropdown.length > 0) {
+        $('a', $nav_dropdown).each(function () {
             var $anchor = $(this);
             var $anchor_icon = $('.icon', $anchor);
             var $menu = $anchor.next('.list--menu');
             var icon_up = $anchor.data('icon-up');
             var icon_down = $anchor.data('icon-down');
 
-            $anchor.on('click', function (e)
-            {
+            $anchor.on('click', function (e) {
                 /**
                  * Prevent click on primary drop-down anchor
                  */
 
-                if ($(this).next().hasClass('list--menu'))
-                {
+                if ($(this).next().hasClass('list--menu')) {
                     e.preventDefault();
                 }
 
                 $menu.toggleClass('is-open');
 
-                if ($menu.hasClass('is-open'))
-                {
+                if ($menu.hasClass('is-open')) {
                     $menu.attr('aria-hidden', 'false');
                     $anchor_icon.removeClass(icon_down).addClass(icon_up);
                 }
-                else
-                {
+                else {
                     $menu.attr('aria-hidden', 'true');
                     $anchor_icon.removeClass(icon_up).addClass(icon_down);
                 }
@@ -196,16 +177,13 @@
 
     var $tabs_component = $('.js-tabs');
 
-    if ($tabs_component.length > 0)
-    {
-        $tabs_component.each(function ()
-        {
+    if ($tabs_component.length > 0) {
+        $tabs_component.each(function () {
             var $tabs = $(this);
             var $tablist = $('.tab-list', $tabs);
             var $tablist_link = $('a', $tablist);
 
-            $tablist_link.on('click', function (e)
-            {
+            $tablist_link.on('click', function (e) {
                 e.preventDefault();
 
                 var $link = $(this);
@@ -224,8 +202,7 @@
                  * Display the selected tab
                  */
 
-                if (!$tab.hasClass('is-selected'))
-                {
+                if (!$tab.hasClass('is-selected')) {
                     $tab.attr('aria-selected', 'true');
                     $tab.addClass('is-selected');
                     $tab_panel.addClass('is-active');
@@ -241,14 +218,11 @@
 
     var $toggle_dropdown_buttons = $('.js-toggle-dropdown-btn');
 
-    if ($toggle_dropdown_buttons.length > 0)
-    {
-        $toggle_dropdown_buttons.each(function ()
-        {
+    if ($toggle_dropdown_buttons.length > 0) {
+        $toggle_dropdown_buttons.each(function () {
             var $button = $('.btn', $(this));
 
-            $button.on('click', function ()
-            {
+            $button.on('click', function () {
                 /**
                  * Open the current dropdown
                  */
@@ -258,21 +232,18 @@
                 var dropdown_id = $(this).data('dropdown-id');
                 var $dropdown = $('#' + dropdown_id);
 
-                if ($dropdown.length > 0)
-                {
+                if ($dropdown.length > 0) {
                     $dropdown.toggleClass('is-open');
 
                     /**
                      * Toggle ARIA attributes
                      */
 
-                    if ($dropdown.hasClass('is-open'))
-                    {
+                    if ($dropdown.hasClass('is-open')) {
                         $dropdown.attr('aria-hidden', 'false');
                         $button.attr('aria-expanded', 'true');
                     }
-                    else
-                    {
+                    else {
                         $dropdown.attr('aria-hidden', 'true');
                         $button.attr('aria-expanded', 'false');
                     }
@@ -283,8 +254,7 @@
 
                     var $dropdowns = $('.dropdown', $toggle_dropdown_buttons);
 
-                    $dropdowns.not($dropdown).each(function ()
-                    {
+                    $dropdowns.not($dropdown).each(function () {
                         $(this).removeClass('is-open');
                         $(this).attr('aria-hidden', 'true');
                     });
@@ -299,10 +269,8 @@
 
     var $toggle_password = $('.js-toggle-password');
 
-    if ($toggle_password.length > 0 && isModernBrowser)
-    {
-        $toggle_password.each(function ()
-        {
+    if ($toggle_password.length > 0 && isModernBrowser) {
+        $toggle_password.each(function () {
             var $form = $(this);
             var $input = $('input[type="password"]', $form);
             var $toggle = $('.js-toggle', $form);
@@ -313,17 +281,14 @@
              * Switch the input type
              */
 
-            $toggle.on('click', function (e)
-            {
+            $toggle.on('click', function (e) {
                 e.preventDefault();
 
-                if ($input.attr('type') === 'password')
-                {
+                if ($input.attr('type') === 'password') {
                     $toggle.html('Hide');
                     $input.attr('type', 'text');
                 }
-                else
-                {
+                else {
                     $toggle.html('Show');
                     $input.attr('type', 'password');
                 }
@@ -335,8 +300,7 @@
              * Reset the input type
              */
 
-            $form.on('submit', function ()
-            {
+            $form.on('submit', function () {
                 $input.attr('type', 'password');
             });
         });
@@ -348,10 +312,8 @@
 
     var $tooltips = $('.js-tooltip');
 
-    if ($tooltips.length > 0)
-    {
-        $tooltips.each(function ()
-        {
+    if ($tooltips.length > 0) {
+        $tooltips.each(function () {
             $(this).tooltip();
         });
     }
@@ -360,8 +322,7 @@
      * Resize
      * ========================================================================== */
 
-    $(window).on('resize', function ()
-    {
+    $(window).on('resize', function () {
         renderMapEmbed();
     });
 })();
